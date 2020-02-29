7f0b5d7d953e1c3aab981428f38996319fef8598
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-03-01 00:24:56 +0800

    add update time

--- api/src/server/controllers/virus.controller.js
+++ api/src/server/controllers/virus.controller.js
@@ -7,7 +7,7 @@ const test = (req, res) => {
 
 const dataGet = (req, res) => {
   request({
-    url: 'https://news.campaign.yahoo.com.tw/2019-nCoV/index.php', // 中央氣象局網頁
+    url: 'https://news.campaign.yahoo.com.tw/2019-nCoV/index.php',
     method: 'GET'
   }, (error, response, body) => {
     if (error || !body) {
@@ -19,6 +19,8 @@ const dataGet = (req, res) => {
     const cheerioDataTitle = $('.dataTitle'); // 爬最外層的 Table(class=BoxTable) 中的 tr
     const cheerioCurrent = $('.current');
     const cheerioMoreThan = $('.moreThan');
+    const cheerioUpdateTime = $('.time').text().trim().split(/([0-9]+)/);
+    const updateTime = `${cheerioUpdateTime[1]}/${cheerioUpdateTime[3]}/${cheerioUpdateTime[5]} ${cheerioUpdateTime[7]}:${cheerioUpdateTime[9]}`;
     // const china_conform=table_tr.find('.box _china');
     for (let i = 0; i < 3; i += 1) {
       const title = cheerioDataTitle.eq(i).text().trim();
@@ -34,10 +36,9 @@ const dataGet = (req, res) => {
       const death = Object.assign({ title, currentCount, morethan });
       deathResult.push(death);
     }
-    const result = Object.assign({ confirm: confirmResult, death: deathResult });
+    const result = Object.assign({ confirm: confirmResult, death: deathResult, updateTime });
     // console.log(confirmResult);
     // console.log(deathResult);
-    console.log(result);
     res.send(result);
   });
 };
@@ -47,37 +48,3 @@ export default {
   dataGet
 };
 
-
-request({
-  url: 'https://news.campaign.yahoo.com.tw/2019-nCoV/index.php', // 中央氣象局網頁
-  method: 'GET'
-}, (error, response, body) => {
-  if (error || !body) {
-    return;
-  }
-  const $ = cheerio.load(body); // 載入 body
-  const confirmResult = []; // 建立一個儲存結果的容器
-  const deathResult = []; // 建立一個儲存結果的容器
-  const cheerioDataTitle = $('.dataTitle'); // 爬最外層的 Table(class=BoxTable) 中的 tr
-  const cheerioCurrent = $('.current');
-  const cheerioMoreThan = $('.moreThan');
-  // const china_conform=table_tr.find('.box _china');
-  for (let i = 0; i < 3; i += 1) {
-    const title = cheerioDataTitle.eq(i).text().trim();
-    const current = cheerioCurrent.eq(i).text().trim();
-    const morethan = cheerioMoreThan.eq(i).text().trim();
-    const confirm = Object.assign({ title, current, morethan });
-    confirmResult.push(confirm);
-  }
-  for (let i = 3; i < 6; i += 1) {
-    const title = cheerioDataTitle.eq(i).text().trim();
-    const current = cheerioCurrent.eq(i).text().trim();
-    const morethan = cheerioMoreThan.eq(i).text().trim();
-    const death = Object.assign({ title, current, morethan });
-    deathResult.push(death);
-  }
-  const result = Object.assign({ confirm: confirmResult, death: deathResult });
-  // console.log(confirmResult);
-  // console.log(deathResult);
-  console.log(result);
-});
--- api/src/server/routes/index.route.js
+++ api/src/server/routes/index.route.js
@@ -1,6 +1,5 @@
 import express from 'express';
 // Router
-import mqtt from './mqtt.route';
 import virus from './virus.route';
 import config from './../../config/config';
 
@@ -12,9 +11,7 @@ router.get('/', (req, res) => {
   res.send(`server started on PORT ${config.port} (${config.env})`);
 });
 
-/** User Router */
+/** virus Router */
 router.use('/virus', virus);
-/** Student Router */
-router.use('/mqtt', mqtt);
 
 export default router;
