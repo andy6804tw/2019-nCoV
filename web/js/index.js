6072ba90fd455cabf33a9b1a5338a7dd0af32bf3
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-03-07 11:20:51 +0800

    add news section

--- web/js/index.js
+++ web/js/index.js
@@ -34,7 +34,7 @@ axios.get(`https://novel-coronavirus-2019.herokuapp.com/virus`)
             const deathList = dataObject['death'];
             const epidemicList = confirmList.concat(deathList);
             const numberDom = document.getElementsByClassName('number');
-            
+
             for (let i = 0; i < 6; i += 1) {
                 numberDom[i].innerHTML = `<span class="number-animate" data-end-value="${epidemicList[i].currentCount}" data-increment="1">0</span><br>${epidemicList[i].morethan}`;
             }
@@ -48,26 +48,60 @@ axios.get(`https://novel-coronavirus-2019.herokuapp.com/virus`)
         }
     );
 
-    // 旅遊等級警示
-    axios.get(`https://novel-coronavirus-2019.herokuapp.com/virus/countryEpidLevel`)
-        .then((response) => {
-                var dataObject = response.data[0].result;
-                const list = document.getElementById('countryEpidLevel');
-                console.log(dataObject)
-                // render DOM
-                for(let i=0;i<dataObject.length;i++){
-                    console.log(dataObject[i]);
-                    const item = document.createElement('tr');
-                    item.innerHTML = `
+// 旅遊等級警示
+axios.get(`https://novel-coronavirus-2019.herokuapp.com/virus/countryEpidLevel`)
+    .then((response) => {
+            var dataObject = response.data[0].result;
+            const list = document.getElementById('countryEpidLevel');
+            // render DOM
+            for (let i = 0; i < dataObject.length; i++) {
+                const item = document.createElement('tr');
+                item.innerHTML = `
                     <td> ${dataObject[i].region} </td> 
                     <td> ${dataObject[i].country} </td> 
                     <td> ${dataObject[i].level} </td>
                     <td > ${dataObject[i].updateTime} </td>
                         `;
-                    list.appendChild(item);
-                }
-            },
-            (error) => {
-                var message = error.response.data.message;
+                list.appendChild(item);
             }
-        );
+        },
+        (error) => {
+            var message = error.response.data.message;
+        }
+    );
+
+// 旅遊等級警示
+axios.get(`https://api.coronatracker.com/news/trending?limit=5&offset=0&countryCode=&country=&language=zh_TW`)
+    .then((response) => {
+            var dataObject = response.data.items;
+            const newsItem = document.getElementById('newsItem');
+            console.log(dataObject)
+            for(let i=0;i<dataObject.length;i++){
+                const postTime = moment.utc(dataObject[i].publishedAt).local().format('YYYY-MM-DD HH:mm:ss');
+                console.log(postTime);
+                const item = document.createElement('a');
+                item.classList = 'card col-12 my-3';
+                item.href = dataObject[i].url;
+                item.target='_blank';
+                item.innerHTML = `
+                    <div class="row py-4">
+                    <div class="card-image col-2 text-center">
+                        <img src="${dataObject[i].urlToImage}" alt="" style="width: 80%;">
+                    </div>
+                    <div class="col-9">
+                        <h5>${dataObject[i].title}</h5>
+                        <p>
+                        ${dataObject[i].description}
+                        </p>
+                        <div class="text-right">${postTime}</div>
+                    </div>
+                    </div>
+                        `;
+                newsItem.appendChild(item);
+                console.log(i)
+            }
+        },
+        (error) => {
+            var message = error.response.data.message;
+        }
+    );
